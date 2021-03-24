5f0ef6360f2cf309389791071c5d7a5b74732b8e
Jowooseok <whdntjrsla12@naver.com>
2021-03-24 11:20:00 +0900

    add layout

--- handhouse/lib/main.dart
+++ handhouse/lib/main.dart
@@ -35,13 +35,176 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 2;
 
   List<Widget> _widgetOptions = [
     //각각의 화면 구성
     Text('hi1'),
     Text('hi2'),
-    Text('hi3'),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Row(
+            children: [
+              Expanded(
+                  child: Icon(
+                Mdi.bedDoubleOutline,
+                size: 40.0,
+                color: Colors.grey,
+              )),
+              Expanded(
+                child: Icon(
+                  Mdi.weatherSunny,
+                  size: 40.0,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amberAccent.shade100,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent.shade100,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            children: [
+              Expanded(
+                child: Icon(
+                  Mdi.windowOpenVariant,
+                  size: 35.0,
+                  color: Colors.grey,
+                ),
+              ),
+              Expanded(
+                child: Icon(
+                  Mdi.airConditioner,
+                  size: 35.0,
+                  color: Colors.grey,
+                ),
+              ),
+              Expanded(
+                child: Icon(
+                  Mdi.waterBoiler,
+                  size: 35.0,
+                  color: Colors.grey,
+                ),
+              ),
+              Expanded(
+                child: Icon(
+                  Mdi.airHumidifier,
+                  size: 35.0,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            children: [
+              Expanded(
+                child: ConstrainedBox(
+                  constraints:
+                      BoxConstraints.tightFor(width: 40.0, height: 40.0),
+                  child: ElevatedButton(
+                    child: Text(
+                      'ON',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      primary: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints:
+                      BoxConstraints.tightFor(width: 40.0, height: 40.0),
+                  child: ElevatedButton(
+                    child: Text(
+                      'OFF',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      shape: CircleBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints:
+                      BoxConstraints.tightFor(width: 40.0, height: 40.0),
+                  child: ElevatedButton(
+                    child: Text(
+                      'ON',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: CircleBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints:
+                      BoxConstraints.tightFor(width: 40.0, height: 40.0),
+                  child: ElevatedButton(
+                    child: Text(
+                      'OFF',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      shape: CircleBorder(),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
     Text('hi4'),
     Text('hi5'),
   ];
